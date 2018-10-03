51c0b30abd3fd44bbe646d4d8d0f8e61706bc405
TaiTT <totai9702@gmail.com>
2018-10-03 11:31:19 +0700

    fix lỗi

--- auto.ino
+++ auto.ino
@@ -1,57 +1,58 @@
 #include "AutoDIY.h"
 
-//============dinh nghia su dung LCD1602====================        
-                       
+//============dinh nghia su dung LCD1602====================
+
 int ntcount, pos;
 
 //=====================================================================
 
-void setup(void){                               
-
-  put_text("ROBOTIC-STARTER", 0, 0);
-  delay(1000); 
-  lcd_clear();
-  put_text("AUTODIY", 0, 0); 
-  if (digitalRead(9) == 0) { 
-    put_text(" SAN TRANG", 0, 0);
-  } else { 
-    put_text(" SAN DEN ", 0, 0); 
+void setup(void) {
+  lcd.begin(16, 2);
+  lcd.print("ROBOTIC-STARTER");
+  delay(1000);
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("AUTODIY");
+  if (digitalRead(9) == 0) {
+    lcd.print(" SAN TRANG");
+  } else {
+    lcd.print(" SAN DEN ");
   }
-  
+
   init_pin();
   init_adc();
   get_adc();
-  set_ngatu(0);
+  ngatu = 0;
   clearr();
   ntcount = 0;
-  //display_adc(ng_adc[0],ng_adc[1],ng_adc[2],ng_adc[3]);
+  display_adc();
 }
-void loop(void){
-  
+void loop(void) {
+
   set_adc();
-  
+
   int i = 0;
-  
-  if(digitalRead(CT1) == 0) {
-    while(true){ 
+
+  if (digitalRead(CT1) == 0) {
+    while (true) {
       i++;
-      do_line(85, 60, 220);
-      
-      if (get_ngatu() >= 1) {
-        set_ngatu(0);
+      do_line(80, 50, 200);
+
+      if (ngatu >= 1) {
+        ngatu = 0;
         control_motor(0, 0, 0);
         control_motor(1, 0, 0);
         break;
       }
-      
-//      if (get_distance() <= 15) {
-//        ngatu = 0;
-//        control_motor(0, 0, 0);
-//        control_motor(1, 0, 0);
-//        servo.write(10);
-//        delay(1000);
-//        servo.write(150);
-//      }
+
+      //      if (get_distance() <= 15) {
+      //        ngatu = 0;
+      //        control_motor(0, 0, 0);
+      //        control_motor(1, 0, 0);
+      //        servo.write(10);
+      //        delay(1000);
+      //        servo.write(150);
+      //      }
     }
   }
 }
